ec710dcbc98480032af54235d1cf554db4b17a53
Mr.Li <liyangyang08144@foxmail.com>
2021-11-29 22:09:56 +0800

    nacos测试

--- service/service_hosp/src/main/java/com/yygh/service_hosp/ResController.java
+++ service/service_hosp/src/main/java/com/yygh/service_hosp/ResController.java
@@ -1,14 +1,45 @@
 package com.yygh.service_hosp;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-public class ResController {
-    @RequestMapping("/login")
-    public String clsj() {
-        return "hello-world";
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
+@RestController
+@RefreshScope
+public class ResController {
+    @Value("${user.userName}")
+    String userName;
+    @RequestMapping(value="/login")
+    public String clsj(@RequestParam(defaultValue = "${user.age}") String userName) {
+
+        System.out.println(userName);
+        return userName;
+
+    }
+
+    public static void main(String[] args) {
+        List<Integer> list= new ArrayList<>();
+        for(int i =0 ; i<100;i++){
+            list.add(i);
+        }
+        long count = list.stream().filter(i -> {
+//            System.out.println("foreach");
+            return i % 3 == 1;
+        }).count();
+        System.out.println(count);
+        Set<Integer> collect = list.stream().filter(v->v%3==1).collect(Collectors.toSet());
+        System.out.println(collect);
+        assert list==collect;
+
+        System.out.println(Stream.of("a", "b", "c").map(v -> v.toUpperCase()).collect(Collectors.toSet()));
     }
 }
